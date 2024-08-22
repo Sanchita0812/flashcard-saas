b84b46aa420accef02917d2aa6f08334d03e0eb9
Sanchita0812 <sanchitakiran08@gmail.com>
2024-08-22 22:39:48 +0530

    .

--- app/sign up/[[...sign up]]/page.js
+++ app/sign up/[[...sign up]]/page.js
@@ -1,5 +1,32 @@
-import { SignUp } from '@clerk/nextjs'
+export default function SignUpPage(){
+  return(
+  <Container maxWidth="100vw">
+      <AppBar position="static" sx={{backgroundColor: theme.palette.primary.dark}}>
+          <Toolbar>
+              <Typography variant="h6" sx={{flexGrow: 1}}>
+                <Image src={SmallLogo} alt="Flasher.io Logo" width={25} sx={{textAlign: "center"}}/>
+              </Typography>
+              <Button color="inherit" sx={{color: theme.palette.primary.contrastText}}>
+              <Link href="/sign-in" passHref/>
+                  Login
+              </Button>
+          </Toolbar>
+      </AppBar>
 
-export default function SignUpPage() {
-  return <SignUp />;
+
+      <Box
+          display="flex"
+          flexDirection="column"
+          justifyContent="center"
+          alignItems="center"
+          sx={{textAlign: 'center', my: 4}}
+          >
+          <Typography variant="h4" component="h1" gutterBottom>
+              Sign Up
+          </Typography>
+          <SignUp />
+      </Box>
+  </Container>
+  );
 }
+
